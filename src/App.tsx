1962fde959fb5b1932a9eb74dafadb582d94e418
Barbosa885 <80699703+Barbosa885@users.noreply.github.com>
2022-08-28 17:06:05 -0300

    :sparkles:Add the page routes in the app and begins the navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Components
+import { Container } from "react-bootstrap";
+import { Navbar } from "./components/Navbar"
+
+// Pages
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
 
 function App() {
+
   return (
-    <h1>
-    Hi
-    </h1>
+  <>
+  <Navbar />
+  <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Container>
+  </>
   )
+
 }
 
-export default App
+export default App;
